a53e9fbdd43c49f95263b8c8fb30ba66e4849a68
Jet Chang <contact@jetchang.com>
2020-06-11 22:53:04 +0200

    Update the document objects

---
+++ app.js
@@ -0,0 +1,10 @@
+// Select DOM
+const input = document.querySelector("#input");
+const submit = document.querySelector("#submit");
+const list = document.querySelector(".todo-list");
+
+console.log(submit);
+console.log(input);
+console.log(list);
+
+// Event Listener
